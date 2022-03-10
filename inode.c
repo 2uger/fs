949454199f79ac5596f2c9c5f6efef37b7c575d3
Ignatenko Oleg <oleg.ignatenko12@gmail.com>
2022-03-10 10:15:57 +0300

    rename, remove dev argument, check code for allocating block through bitmap

--- inode.c
+++ inode.c
@@ -1,103 +1,92 @@
-// Simple file system
-// Basic structure on disk:
-// SPB :: INODES :: BITMAP :: BLOCKS
+/*
+ * Simple file system
+ * Basic structure on disk:
+ * SPB :: INODES :: BITMAP :: BLOCKS
+ */
 
 #include <stddef.h>
 #include <stdint.h>
+#include <string.h>
+#include <stdio.h>
 
 #include "buf.h"
-#include "fs.h"
-#include "defs.h"
+#include "inode.h"
 
-#define min(a, b) (a < b ? a : b)
 
 struct spblock spb;
 
-// fun function to init fs by write superblock
-void
-init_fs(uint32_t dev)
-{
-    struct CacheBuffer *buf;
-    int m = 9999; 
-    buf = bread(dev, 0);
-    mmemmove(buf->data, &m, 4);
-    spb.magic = 9999;
-    spb.size = BLOCKS_NUM * BLOCK_SIZE;
-    spb.data_blocks_num = DATA_BLOCKS_NUM;
-    spb.inodes_num = INODES_NUM;
-    spb.inodes_start = 4;
-    spb.bitmap_start = 7;
-
-    bwrite(buf);
-}
 
 void
-readspblock(uint32_t dev, struct spblock *spb)
+readspblock(struct spblock *spb)
 {
     struct CacheBuffer *b;
-    b = bget(dev, 0);
-    kprintf("Read from first block %s\n", b->data);
-    mmemmove(spb, b->data, sizeof(struct spblock));
+    b = bread( 0);
+    memmove(spb, b->data, sizeof(struct spblock));
     brelease(b);
 }
 
 static void
-zeroblock(uint32_t dev, uint32_t blockn)
+zeroblock(int blockn)
 {
     struct CacheBuffer *b;
-    b = bget(dev, blockn);
-    mmemset(b, 0, BLOCK_SIZE);
+    b = bread(blockn);
+    memset(b, 0, BLOCK_SIZE);
     bwrite(b);
     brelease(b);
 }
-
 
 // Functions to work with blocks on disk
 // balloc - allocating block on disk
 //          setup right bitmap bit
 // bfree - clean disk data block by setting bitmap bit into zero
 
-// allocating new block on disk
-uint32_t
-balloc(uint32_t dev)
+/*
+ * allocating new block on disk
+ */
+int
+balloc()
 {
-    uint32_t m, b, bm;
+    int m, b, bm;
     struct CacheBuffer *buf;
 
-    for (b = 1; b < spb.size; b+=BPB) {
-        // if block's size 8 => we get 64 bits as bitmap
-        // get block of 8*8 bits showing status of blocks
-        buf = bread(dev, BMBLOCK(b, spb));
-        kprintf("Read %d\n", BMBLOCK(b, spb));
+    // start from 1 to skip super block
+    // incrementing by BPB to skip receiving the same bitmap block
+    for (b = 1; b < spb.size; b += BPB) {
+        // get block of BPB*BLOCK_SIZE bits showing status of blocks
+        buf = bread(BMBLOCK(b, spb));
         // iterating through byte till BPB or untill blocks amount on disk
         for (bm = 0; bm < BPB || bm + b < spb.size; bm++) {
-            // actually we iterating through bits in byte(char)
+            // actually we iterating through "bits in byte"
             m = 1 << (bm % 8);
-            if ((buf->data[bm/8] & m) == 0) { // is block free
-                buf->data[bm/8] |= m; // mark block as not free
+            // is block free
+            if ((buf->data[bm/8] & m) == 0) {
+                // mark block as not free
+                buf->data[bm/8] |= m;
                 bwrite(buf);
                 brelease(buf);
-                zeroblock(dev, b + bm);
+                zeroblock(b + bm);
                 return b + bm; // block number that we allocate
             }
         }
         brelease(buf);
     }
-    panic("out of disk block");
+    printf("ERROR: out of disk block\n");
 }
 
-// freeing disk block
+/*
+ * mark block as free in corresponding bitmap by block number
+ */
 static void
-bfree(uint32_t dev, uint32_t block_num) {
-    // find bitmap bit for current block
-    // make it 0
+bfree(int blockn) {
     struct CacheBuffer *b;
-    b = bread(dev, (BMBLOCK(block_num, spb)));
+    // bitmap block
+    b = bread((BMBLOCK(block_num, spb)));
 
-    uint32_t bm = block_num % BPB;
-    uint32_t m = 1 << (bm % 8);
-    if (b->data[bm/8] & m == 0) // already free
-        panic("freeing free block");
+    int bm = block_num % BPB;
+    int m = 1 << (bm % 8);
+
+    if (b->data[bm/8] & m == 0)
+        printf("ERROR: try to free block, that already free");
 
     // 000-1-000 -> 000-0-000
     b->data[bm/8] &= ~m;
---
+++ inode.h
@@ -0,0 +1,64 @@
+#ifndef FS_H
+#define FS_H
+
+#include <stdint.h>
+
+#include "param.h"
+
+
+#define INODES_PER_BLOCK (BLOCK_SIZE / sizeof(struct dinode))
+
+// disk block containing inode
+#define IBLOCK(i, sb) ((i/INODES_PER_BLOCK) + sb.inodes_start)
+
+// bit per block
+#define BPB (BLOCK_SIZE * 8) // as we measure block size in bytes
+
+// disk block containing bitmap bit for demanding block
+#define BMBLOCK(b, spb) ((b/BPB) + spb.bitmap_start)
+
+#define min(a, b) (a < b ? a : b)
+
+
+// superblock
+struct spblock {
+    int magic; // FS magic number, used to recognize file system
+    int size;  // FS size, measured in blocks amount
+
+    int data_blocks_num;
+    int inodes_num;
+
+    int inodes_start; // block where inodes start
+    int bitmap_start; // block where bitmap start
+}__attribute__((packed));
+
+// disk inode structure
+struct dinode {
+    int type;  // identify type of information(file, dir, device). 0 means free.
+    int nlink; // how much dirs link to that file
+    int size;  // how much data in file
+    int addrs[DATA_BLOCKS_NUM]; // addresses of all file's blocks
+}__attribute__((packed));
+
+// memory inode structre
+struct inode {
+    int inum;  // inode number
+    int ref;   // count all reference from C code(pointers), remove when 0
+    int valid; // usage: when find free place in inode table, but dont read from disk
+
+    // same structure as on disk inode
+    int type; 
+    int nlink;
+    int size;
+    int addrs[DATA_BLOCKS_NUM];
+}__attribute__((packed));
+
+// directory is a file containing links to file
+struct dirent {
+    char name[MAX_FILE_NAME];
+    int inum;
+}__attribute__((packed));
+
+enum InodeType {FILE=1, DIRECTORY, DEVICE};
+
+#endif
