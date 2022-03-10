c643112db049fcd2b7c9a28481d3577857b7fd20
Ignatenko Oleg <oleg.ignatenko12@gmail.com>
2022-03-10 10:16:18 +0300

    create header file to stope all file system parameters

---
+++ param.h
@@ -0,0 +1,17 @@
+#ifndef PARAM_H
+#define PARAM_H
+
+// disk emulator
+#define BLOCK_SIZE 512
+#define BLOCKS_NUM 512
+
+// cache buffers
+#define BCACHE_NUM 64
+
+// inodes
+
+// how much data blocks inode could addressing
+#define DATA_BLOCKS_NUM 16
+#define MAX_FILE_NAME 16
+
+#endif
