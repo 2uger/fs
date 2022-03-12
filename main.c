499cc92f90e1c4ba85beabaadec0b4608bc7f922
Ignatenko Oleg <oleg.ignatenko12@gmail.com>
2022-03-12 09:27:38 +0300

    write more tests

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <string.h>
+#include <assert.h>
 
 #include "inode.h"
 #include "dir.h"
@@ -31,16 +32,39 @@ main()
 
     fs_init();
 
-    char file_name[20] = "new_file";
-    char file_input[100] = "File input was written by ME";
+    char file_name[] = "new_file";
+    char file_input_1[] = "File input was written by ME";
     char file_output[100];
 
+    // simple write into clean file
     creat(file_name);
-    write(file_name, file_input, sizeof(file_input));
-    read(file_name, file_output, 0, sizeof(file_input));
+    write(file_name, file_input_1, sizeof(file_input_1));
+    read(file_name, file_output, 0, sizeof(file_input_1));
 
-    printf("Reading from file: %s\n", file_output);
+    assert(!strcmp(file_input_1, file_output));
 
+    char file_input_2[] = "New file input";
+
+    // write into file with data
+    write(file_name, file_input_2, sizeof(file_input_2));
+    read(file_name, file_output, 0, sizeof(file_input_2));
+
+    // check first data was not erased
+    assert(!strcmp(file_input_1, file_output));
+
+    read(file_name, file_output, sizeof(file_input_1), sizeof(file_input_2));
+    // check new data was written
+    assert(!(strcmp(file_input_2, file_output)));
+
+    struct inode *file = dirlookup(root_dir, file_name);
+    assert(file->size == sizeof(file_input_1) + sizeof(file_input_2));
+
+    // return 0 if no such file exists
+    assert(!dirlookup(root_dir, "random_name"));
+    // such file already exists
+    assert(dirlink(root_dir, file_name, 123) == -1);
+
+    printf("All tests passed!!!\n");
     return 0;
 }
 
